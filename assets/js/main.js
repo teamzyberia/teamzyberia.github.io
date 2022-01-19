4902279f8d1cad62d7578ce190979f73ecd19e30
zervbot <asanomo@outlook.jp>
2022-01-19 12:10:21 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,11 @@
 function initial() {
     console.log("Welcome to Team Zyberia.");
 }
+
+function invite(email){
+    const token = "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";
+    const org = "teamzyberia";
+    axios.defaults.headers.common["Accept"] = "application/vnd.github.v3+json";
+    axios.defaults.headers.common["Authorization"] = `token ${token}`;
+    axios.post(`https://api.github.com/orgs/${org}/invitations`, { email: email });
+}
